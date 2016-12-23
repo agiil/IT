84d2eda655f099c3feff3e9d6abab0fcaaba81b1
Priit Parmakson <priit.parmakson@gmail.com>
2016-12-23 13:03:00 +0200

    Lisandus

--- js/Utils.js
+++ js/Utils.js
@@ -6,7 +6,6 @@ function init() {
   // Bind toggling handler to submenu switches
   $('.lyliti').click(function() {
     var i = $(this).attr('id');
-    alert('id=' + i);
     $('#alammenyy' + i).toggle();
   });
 }
