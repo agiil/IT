72d8572f43cc61daa6bc06cbab37fb4788f833a2
Priit Parmakson <priit.parmakson@gmail.com>
2016-12-23 12:02:12 +0200

    Lisandus

--- js/Utils.js
+++ js/Utils.js
@@ -1,26 +1,5 @@
 
 function lylita() {
-  alert('parent id:' + $(this).parent().attr('id'));
-  var see = $(this).parent().find('ul').first();
-  alert('eesmärk leitud');
-  if (see.hasClass('closed')) {
-    alert('omab klassi closed');
-  }
-  else {
-    alert('ei oma klassi closed');
-  }
-  alert(JSON.stringify(see));
-  alert('nägid?');
-  alert($(this).css('color'));
-  $(this).css('backgroundColor', 'Blue');
-  $(this).parent().addClass('closed');
-  return
-  var alam = $(this).parent().find('ul').first();
-  alert('2');
-  if (alam.hasClass('closed')) {
-    alam.removeClass('closed')
-  }
-  else {
-    alam.addClass('closed')
-  }
+  $('#alamlist1').removeClass('closed');
+  alert('klass closed eemaldatud');
 }
