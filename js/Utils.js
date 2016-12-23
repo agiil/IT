7a11f97e768bbd678673baef8531ee0034ec5b98
Priit Parmakson <priit.parmakson@gmail.com>
2016-12-23 12:58:23 +0200

    Lisandus

--- js/Utils.js
+++ js/Utils.js
@@ -1,12 +1,12 @@
 function init() {
+
+  // Hide all submenus
+  $('.alammenyy').hide();
+
+  // Bind toggling handler to submenu switches
   $('.lyliti').click(function() {
     var i = $(this).attr('id');
-    alert('id=' + i);
+    // alert('id=' + i);
+    $('#alammenyy' + i).toggle();
   });
-}
-
-function lylita() {
-  alert('id=' + this.id);
-  $('#alamlist' + this.id).removeClass('closed');
-  alert('klass closed eemaldatud');
 }
