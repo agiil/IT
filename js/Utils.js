e5130cc5851b1dba6711ea7a23f997f7e2828b07
Priit Parmakson <priit.parmakson@gmail.com>
2016-12-23 11:40:44 +0200

    Lisandus

--- js/Utils.js
+++ js/Utils.js
@@ -1,8 +1,10 @@
 
 function lylita() {
   alert('siin');
-  var see = $(this);
-  alert('see leitud');
+  var see = $(this).parent().find('ul').first();
+  alert('eesmärk leitud');
+  alert(JSON.stringify(see));
+  alert('nägid?');
   alert($(this).css('color'));
   $(this).css('backgroundColor', 'Blue');
   $(this).parent().addClass('closed');
